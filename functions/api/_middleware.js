4247db36acdc959a44136218c36015f077fcdd6a
Midokuni <jdkurogami+git@gmail.com>
2023-04-11 22:19:45 +0900

    test middleware

---
+++ functions/api/_middleware.js
@@ -0,0 +1,9 @@
+export async function onRequest(context) {
+	try {
+	  return await context.next();
+	} catch (err) {
+	  return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+	}
+  }
+  
+  
