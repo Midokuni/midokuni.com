e510b33861be53bf59f71c4f96c83c05d697aad8
Midokuni <jdkurogami+git@gmail.com>
2023-04-11 22:27:40 +0900

    cors header

--- functions/api/_middleware.js
+++ functions/api/_middleware.js
@@ -1,6 +1,8 @@
 export async function onRequest(context) {
 	try {
-	  return await context.next();
+		let res = await context.next();
+		res.headers.set('Access-Control-Allow-Origin', '*');
+		return res;
 	} catch (err) {
 	  return new Response(`${err.message}\n${err.stack}`, { status: 500 });
 	}
